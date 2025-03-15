c53a6d0232affc83ddc6183638306e5c81878454
harshh351998 <mendapara.harsh47@gmail.com>
2025-03-15 20:42:57 +0530

    #07:- Develop layout of login form

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ document.querySelector('#cart-btn').onclick = () =>
 {
         shoppingCart.classList.toggle('active');
 }
+
+let loginForm = document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () =>
+{
+        loginForm.classList.toggle('active');
+}
